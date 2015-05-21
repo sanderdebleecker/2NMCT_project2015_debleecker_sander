452017da20bcde9af6be239bcf0055bc191b673b
sanderdebleecker <sander.de.bleecker@student.howest.be>
2015-05-21 13:28:51 +0200

    Initial commit

---
+++ app/src/main/java/be/howest/nmct/androiddesproject/DesActivity.java
@@ -0,0 +1,13 @@
+package be.howest.nmct.androiddesproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_des);
+    }
+}
---
+++ app/src/main/java/be/howest/nmct/androiddesproject/DesActivityFragment.java
@@ -0,0 +1,23 @@
+package be.howest.nmct.androiddesproject;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DesActivityFragment extends Fragment {
+
+    public DesActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_des, container, false);
+    }
+}
